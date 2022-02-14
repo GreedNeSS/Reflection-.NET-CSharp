abb538128140909cb42cc5fe692be27c47ea7a72
GreedNeSS <ruslan199126@gmail.com>
2022-02-14 21:31:59 +0300

    Добавьте файлы проекта.

---
+++ TypeViewer/CarLibrary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarLibrary
+{
+    public enum EngineState
+    {
+        engineAlive,
+        EngineDead
+    }
+
+    public enum MusicMedia
+    {
+        musicCd,
+        musicTape,
+        musicRadio,
+        musicMp3,
+        musicWiFI
+    }
+
+    public abstract class Car
+    {
+        public string PetName { get; set; }
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+
+        protected EngineState engState = EngineState.engineAlive;
+        public EngineState EngineState => engState;
+        public abstract void TurboBust();
+
+        public Car()
+        {
+            MessageBox.Show("CarLibrary Version 3.0!");
+        }
+
+        public Car(string name, int maxSp, int currSp)
+        {
+            MessageBox.Show("CarLibrary Version 3.0!");
+            PetName = name;
+            MaxSpeed = maxSp;
+            CurrentSpeed = currSp;
+        }
+
+        public void TurnOnRadio(bool musicOn, MusicMedia mm) =>
+            MessageBox.Show(musicOn ? $"Jamming {mm}" : "Quiet time ...");
+    }
+}
---
+++ TypeViewer/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarLibrary;
+
+namespace TypeViewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------- Write Type -------");
+            string typeName;
+
+            do
+            {
+                Console.WriteLine("\nEnter a type name to evaluate");
+                Console.Write("or enter Q to quit: ");
+
+                typeName = Console.ReadLine();
+
+                if (typeName.Equals("Q", StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+
+                Type type = Type.GetType(typeName);
+
+                if (type != null)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+                    ListVariousStats(type);
+                    ListFields(type);
+                    ListProperties(type);
+                    ListMethods(type);
+                    ListInterfaces(type);
+
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, can't find type");
+                }
+                
+            } while (true);
+        }
+
+        static void ListMethods(Type t)
+        {
+            Console.WriteLine("***** Methods *****");
+            var methods = from m in t.GetMethods() select m.Name;
+            foreach (var method in methods)
+                Console.WriteLine($"=> {method}");
+            Console.WriteLine();
+        }
+
+        static void ListFields(Type t)
+        {
+            Console.WriteLine("***** Fields *****");
+            var fields = from f in t.GetFields() select f.Name;
+            foreach (var field in fields)
+                Console.WriteLine($"=> {field}");
+            Console.WriteLine();
+        }
+
+        static void ListProperties(Type t)
+        {
+            Console.WriteLine("***** Properties *****");
+            var properties = from p in t.GetProperties() select p.Name;
+            foreach (var prop in properties)
+                Console.WriteLine($"=> {prop}");
+            Console.WriteLine();
+        }
+
+        static void ListInterfaces(Type t)
+        {
+            Console.WriteLine("***** Interfaces *****");
+            var interfaces = from i in t.GetInterfaces() select i;
+            foreach (var i in interfaces)
+                Console.WriteLine($"=> {i.Name}");
+            Console.WriteLine();
+        }
+
+        static void ListVariousStats(Type t)
+        {
+            Console.WriteLine("***** Various Statistics *****");
+            Console.WriteLine($"Base class is: {t.BaseType}");
+            Console.WriteLine($"Is type abstract? {t.IsAbstract}");
+            Console.WriteLine($"Is type sealed? {t.IsSealed}");
+            Console.WriteLine($"Is type generic? {t.IsGenericTypeDefinition}");
+            Console.WriteLine($"Is type a class type? {t.IsClass}");
+            Console.WriteLine();
+        }
+    }
+}
