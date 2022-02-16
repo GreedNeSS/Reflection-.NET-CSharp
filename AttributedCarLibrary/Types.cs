238632dfd4437ac5ba427fc9531e98050367bbc6
GreedNeSS <ruslan199126@gmail.com>
2022-02-16 23:15:52 +0300

    Added attribute reader example with early binding

---
+++ AttributedCarLibrary/Types.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: CLSCompliant(true)]
+
+namespace AttributedCarLibrary
+{
+    [System.AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false, AllowMultiple = false)]
+    public sealed class VehicleDescriptionAttribute : Attribute
+    {
+        readonly string description;
+
+        // This is a positional argument
+        public VehicleDescriptionAttribute(string vehicalDescription)
+        {
+            this.description = vehicalDescription;
+        }
+
+        public string Description
+        {
+            get { return description; }
+        }
+    }
+
+    [Serializable]
+    [VehicleDescription("My rocking Harley")]
+    public class Motorcycle
+    {
+    }
+
+    [VehicleDescription("A very long, slow, but feature-rich auto")]
+    public class Winnebago
+    {
+    }
+
+    [SerializableAttribute]
+    [ObsoleteAttribute("Use another vehicle!")]
+    [VehicleDescriptionAttribute("The old gray mare, she ain't what she used to be...")]
+    public class HorseAndBuggy
+    {
+    }
+}
---
+++ VehicleDescriptionAttributeReader/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AttributedCarLibrary;
+
+namespace VehicleDescriptionAttributeReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("********** Calue of VehicleDescriptionAttribute **********");
+            ReflectOnAttributesWithEarlyBinding();
+        }
+
+        private static void ReflectOnAttributesWithEarlyBinding()
+        {
+            //Type t = typeof(Winnebago);
+
+            string token = "3F 07 FE CB 6E BD CC 6A".Replace(" ", "");
+            Type t = Type.GetType($"AttributedCarLibrary.Winnebago, AttributedCarLibrary," +
+                $" Version=1.0.0.0, Culture=neutral, PublicKeyToken={token}");
+
+            if (t != null)
+            {
+                object[] customAtts = t.GetCustomAttributes(false);
+
+                foreach (VehicleDescriptionAttribute attribute in customAtts)
+                {
+                    Console.WriteLine($"=> Description: {attribute.Description}");
+                }
+            }
+        }
+    }
+}
