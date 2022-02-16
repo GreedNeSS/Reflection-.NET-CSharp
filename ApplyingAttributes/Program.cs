510afb0799b6438323242da0e914f9bc74884827
GreedNeSS <ruslan199126@gmail.com>
2022-02-16 21:16:44 +0300

    Used attributes

---
+++ ApplyingAttributes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplyingAttributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HorseAndBuggy horseAndBuggy = new HorseAndBuggy();
+        }
+    }
+
+    [Serializable]
+    public class Motorcycle
+    {
+        [NonSerialized]
+        float weigthOfCurrentPassengers;
+        bool hasRadioSystem;
+        bool hasHeadSet;
+        bool hasSissyBar;
+    }
+
+    [Serializable, Obsolete("Use another vehicle!")]
+    public class HorseAndBuggy
+    {
+
+    }
+}
