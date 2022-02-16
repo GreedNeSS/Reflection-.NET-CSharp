f5459aaea01a5f66c782b28a1cadcb3342984bf9
GreedNeSS <ruslan199126@gmail.com>
2022-02-17 00:08:42 +0300

    Added attribute reader example with late binding

--- VehicleDescriptionAttributeReaderLateBinding/Program.cs
+++ VehicleDescriptionAttributeReaderLateBinding/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,12 +11,33 @@ namespace VehicleDescriptionAttributeReaderLateBinding
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("********** Value of VehicleDescriptionAttribute **********");
+            ReflectAttributesUsingLateBinding();
         }
 
         private static void ReflectAttributesUsingLateBinding()
         {
+            try
+            {
+                Assembly asm = Assembly.Load("AttributedCarLibrary");
+                Type vehicleDesc = asm.GetType("AttributedCarLibrary.VehicleDescriptionAttribute");
+                PropertyInfo propDesc = vehicleDesc.GetProperty("Description");
+                Type[] types = asm.GetTypes();
 
+                foreach (Type t in types)
+                {
+                    object[] customAtts = t.GetCustomAttributes(vehicleDesc, false);
+
+                    foreach (object obj in customAtts)
+                    {
+                        Console.WriteLine($"=> {t.Name}: {propDesc.GetValue(obj)}");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
